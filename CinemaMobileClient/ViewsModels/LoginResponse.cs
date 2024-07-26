0a2dad3652dbb9eae2d063740f881cd134955f94
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-07-25 19:32:40 -0600

    Login: - Se agrega funcionalidad de login

---
+++ CinemaMobileClient/ViewsModels/LoginResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaMobileClient.ViewsModels
+{
+    public class LoginResponse
+    {
+        public LoginResponse()
+        {
+            this.data = new LoginData();
+        }
+        public LoginData data { get; set; }
+
+    }
+
+    public class LoginData
+    {
+        public int userId { get; set; }
+        public string message { get; set; } = string.Empty;
+        public int status { get; set; }
+        public string codVerificacion { get; set; } = string.Empty;
+        public string username { get; set; } = string.Empty;
+        public string rol { get; set; } = string.Empty;
+    }
+
+}
