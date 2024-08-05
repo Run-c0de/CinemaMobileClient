31540c5f174811cc987ceda88093287e9a6e2a41
I.Ariel Flores <159189980+Run-c0de@users.noreply.github.com>
2024-08-04 21:03:05 -0600

    QR code generator

--- CinemaMobileClient/Views/ReceiptView.xaml.cs
+++ CinemaMobileClient/Views/ReceiptView.xaml.cs
@@ -1,19 +1,29 @@
-﻿
+﻿using CinemaMobileClient.ViewModels;
+using QRCoder;
+
 namespace CinemaMobileClient.Views;
 
 public partial class ReceiptView : ContentPage
 {
-  public ReceiptView()
-  {
-    InitializeComponent();
+
+    private string getQR(string id) => $"https://ksoriano55.github.io/CinepolisWebApp/#/tickets/{id}";
+    
+    public ReceiptView(VentaViewModel venta)
+    {
+        InitializeComponent();
+        OnGenerateClicked(venta.ventaId.ToString());
+        Comprador.Text = "Comprado";
+        TotalCharge.Text =  $"Total Charge: L. {venta.TotalCharge.ToString()}";
+    }
 
 
-  }
-
-  private void OnCounterClicked(object sender, EventArgs e)
-  {
-
-  }
-
-
+    private void OnGenerateClicked(string id)
+    {
+        
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(getQR(id), QRCodeGenerator.ECCLevel.L);
+        PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
+        byte[] qrCodeBytes = qRCode.GetGraphic(20);
+        QrCodeImage.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+    }
 }
